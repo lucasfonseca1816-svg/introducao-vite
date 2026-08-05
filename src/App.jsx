dabcea98018d330a4aac0c3fbc59e97b0c1579bc
lucasfonseca1816-svg <lucasfonseca1816@gmail.com>
2026-08-05 20:39:38 -0300

    aula5

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import './App.css'
 function App() {
 
   return (
-    <div>
-      <h1>Home</h1>
-    </div>  
+      <div>
+        <h1>Projeto Vite</h1>
+        <h2>Exemplo h2</h2>
+        <h3>Exemplo h3</h3>
+        <p>Exemplo parágrafo</p>
+      </div> 
   )
 }
 
